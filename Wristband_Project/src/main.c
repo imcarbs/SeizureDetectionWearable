1cd846b5924fdc72b8ba162a8ed020907caba1a0
Carlos Hernandez <carbsemail@gmail.com>
2015-06-09 22:46:24 -0700

    Counter is enabled but interrupt handler never triggered. FIX

--- Wristband_Project/src/main.c
+++ Wristband_Project/src/main.c
@@ -20,15 +20,14 @@ void tc_init(void);
 void TC0_Handler(void){
 	
 	//toggle LED @ TC0 Frequency
-	if(pioa_regs->PIO_OSR & PIO_PA6){
+//	if(!(pioa_regs->PIO_PDSR & PIO_PA6)){
+//		pioa_regs->PIO_SODR = PIO_PA6;
+//	}
+//	else{
 		pioa_regs->PIO_CODR = PIO_PA6;
-	}
-	else{
-		pioa_regs->PIO_SODR = PIO_PA6;
-	}
-	
-	
+//	}
 }
+
 //main function
 int main (void)
 {
@@ -55,19 +54,21 @@ void pio_init(void){
 //	pioa_regs->PIO_CODR = PIO_PA6;
 
 	//set LED pin PA6 as high (LED is active low)
-//	pioa_regs->PIO_SODR = PIO_PA6;
+	pioa_regs->PIO_SODR = PIO_PA6;
 }
 
 void tc_init(void){
-	
+		
 	//enable pin for tc periph
-	pioa_regs->PIO_PDR = PIO_PA0;
+	//set mux enable on PIO
+	pioa_regs->PIO_ABCDSR[0] = PIO_ABCDSR_P23;
+	pioa_regs->PIO_ABCDSR[1] = (0x0  << 23);
+	
+	//disable PIO control so TC can control pin
+	pioa_regs->PIO_PDR = PIO_PA23;
 		
 	/*Clock setup*/
-	//Temporarily disable PMC clock for peripheral (ID: 23)
-// 	PMC->PMC_PCDR0 = PMC_PCER0_PID23;
-//	PMC->PMC_PCR = PMC_PCR_CMD | PMC_PCR_PID(23) | PMC_PCR_DIV_PERIPH_DIV_MCK;
-	//enable TC clock (ID: 23)
+	//enable TC clock (ID: 23) in PMC
 	PMC->PMC_PCER0 = PMC_PCER0_PID23;
 	/*End Clock setup*/
 	
@@ -76,13 +77,23 @@ void tc_init(void){
 	tc_regs->TC_CCR = TC_CCR_CLKDIS;
 		
 	//set wave mode and to reset on match
-	tc_regs->TC_CMR = TC_CMR_WAVE | TC_CMR_WAVSEL_UP_RC;
+	//also set/clear muxed pin to toggle on RA & RC match (scope debugging)
+	tc_regs->TC_CMR = 
+		TC_CMR_WAVE
+		| TC_CMR_WAVSEL_UP_RC 
+		| TC_CMR_TCCLKS_TIMER_CLOCK4 // = 8Mhz * (1/128)
+		| TC_CMR_ACPA_SET
+		| TC_CMR_ACPC_CLEAR;
 	
-	//set compare number
-	tc_regs->TC_RC = 0xFF;
+	//set period & duty cycle 
+	tc_regs->TC_RA = 0x00FF; //duty cycle for TIOA
+	tc_regs->TC_RC = 0xF424; //period (for TIOA & TIOB)
 	
 	//enable interrupt on compare match
-	tc_regs->TC_IER = TC_IER_CPCS;	
+	tc_regs->TC_IER = TC_IER_CPCS;
+	
+	//enable tc clock
+	tc_regs->TC_CCR = TC_CCR_CLKEN;	
 	/*End TC0 Setup*/	
 	
 	//Enable Interrupt in NVIC
@@ -90,3 +101,5 @@ void tc_init(void){
 	NVIC_EnableIRQ(TC0_IRQn);
 	
 }
+
+
