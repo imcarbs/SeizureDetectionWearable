9d5fb0d1f88cfc75fc1f5ada1133b0ca26d9619f
imcarbs <carbsemail@gmail.com>
2015-07-21 14:59:04 -0700

    Buzzer (on PA1) sucessfully activated whenever threshold is passed from either channel 0 (EDA sensor) or channel 1 (EMG sensor) on ADC.

--- Wristband_Project/src/main.c
+++ Wristband_Project/src/main.c
@@ -13,8 +13,8 @@ Pio *pioa_ptr = PIOA;
 Tc *tc_ptr = TC0;
 Adc *adc_ptr = ADC;
 
-int test_value = 0;
-
+int eda_voltage = 0;
+int emg_voltage = 0;
 
 //function prototypes
 void enable_pio_clk(void);
@@ -26,48 +26,30 @@ void tc_init(void);
 void adc_init(void);
 
 void TC0_Handler(void){
-
-	//check if button is pressed
-	if(pioa_ptr->PIO_PDSR & PIO_PA2){
-		
-		//get adc channel 1current reading
-		adc_ptr->ADC_CR = ADC_CR_START;
-		test_value = adc_ptr->ADC_CDR[1];
-		
-		//check if ADC reads more than(600/4095)*Vcc, light up LED
-		if( test_value > 600 ){
+	
+//check if button is pressed
+//if(pioa_ptr->PIO_PDSR & PIO_PA2)	
+	
+	//get adc channel 0 & 1 current reading
+	adc_ptr->ADC_CR = ADC_CR_START;
+	eda_voltage = adc_ptr->ADC_CDR[0];
+	emg_voltage = adc_ptr->ADC_CDR[1];
+	
+	//check if ADC reads more than threshold, turn on buzzer
+	if( (eda_voltage > 3000) || (emg_voltage > 3000) ){
 			
-			//set LED pin PA6 as low (LED is active low)
-			pioa_ptr->PIO_CODR = PIO_PA6;
-			test_value = 0;
-		}
-		
-		else{
-			
-			//set LED pin PA6 as high (LED is active low)
-			pioa_ptr->PIO_SODR = PIO_PA6;
-			test_value = 0;
-		}	
+		//set PA1 to toggle (buzzer is turned on)
+//		tc_ptr->TC_CHANNEL[0].TC_CMR |= TC_CMR_BCPB_SET;		//set PA1 on RB match
+		tc_ptr->TC_CHANNEL[0].TC_RB = 7500; // 50% duty cycle for TIOB (max buzzer volume)
+		eda_voltage = 0;
+		emg_voltage = 0;
 	}
+	
 	else{
-		//get adc channel 0 current reading
-		adc_ptr->ADC_CR = ADC_CR_START;
-		test_value = adc_ptr->ADC_CDR[0];
-			
-		//check if ADC reads more than (2000/4095)*Vcc, light up LED
-		if( test_value > 2000 ){
-				
-			//set LED pin PA6 as low (LED is active low)
-			pioa_ptr->PIO_CODR = PIO_PA6;
-			test_value = 0;
-		}
-			
-		else{
-				
-			//set LED pin PA6 as high (LED is active low)
-			pioa_ptr->PIO_SODR = PIO_PA6;
-			test_value = 0;
-		}		
+//		tc_ptr->TC_CHANNEL[0].TC_CMR |= TC_CMR_BCPB_CLEAR;		//clr. PA1 on RB match
+		tc_ptr->TC_CHANNEL[0].TC_RB = 0x0000; //0% duty cycle for TIOB (buzzer off)
+		eda_voltage = 0;
+		emg_voltage = 0;
 	}
 	
 	//clear flag
@@ -182,17 +164,17 @@ void tc_init(void){
 	tc_ptr->TC_CHANNEL[0].TC_CMR = 
 		TC_CMR_WAVE						//set TC for wave mode
 		| TC_CMR_WAVSEL_UP_RC			//count up to RC value
-		| TC_CMR_TCCLKS_TIMER_CLOCK4	// = 8Mhz (Master Clock) * (1/128)
+		| TC_CMR_TCCLKS_TIMER_CLOCK1	// = 120Mhz (Master Clock) * (1/2)
 		| TC_CMR_EEVT_XC0				//set xc0 so TIOB is not used as input
 		| TC_CMR_ACPA_SET				//set PA0 on RA match
 		| TC_CMR_ACPC_CLEAR				//clear PA0 on RC match
 		| TC_CMR_BCPB_SET				//set PA1 on RB match
-		| TC_CMR_BCPC_CLEAR;			//clear PA0 on RC match
+		| TC_CMR_BCPC_CLEAR;			//clear PA1 on RC match
 	
-	//set period & duty cycle 
-	tc_ptr->TC_CHANNEL[0].TC_RA = 0x17FF; //duty cycle for TIOA
-	tc_ptr->TC_CHANNEL[0].TC_RB = 0x17FF; //duty cycle for TIOB
-	tc_ptr->TC_CHANNEL[0].TC_RC = 0x2FFF; //period (for TIOA & TIOB)
+	//set period & duty cycle (RC value = (120Mhz * prescaler)/(goal frequency))
+	tc_ptr->TC_CHANNEL[0].TC_RA = 7500; //duty cycle for TIOA
+	tc_ptr->TC_CHANNEL[0].TC_RB = 7500; //duty cycle for TIOB
+	tc_ptr->TC_CHANNEL[0].TC_RC = 15000; //period (for TIOA & TIOB)
 	
 	//enable interrupt on RC compare match
 	tc_ptr->TC_CHANNEL[0].TC_IER = TC_IER_CPCS;
@@ -210,7 +192,7 @@ void tc_init(void){
 
 void adc_init(void){
 	
-	//set prescaler for ADC clk
+	//set prescaler for ADC clk, track time to max., and set MR Transfer to 2 (default)
 	adc_ptr->ADC_MR = ADC_MR_TRACKTIM(490) | ADC_MR_TRANSFER(2) | ADC_MR_PRESCAL(255);
 	
 	//enable ADC channel 0 (PA17 - EDA sensor)
@@ -220,13 +202,6 @@ void adc_init(void){
 	adc_ptr->ADC_CHER = ADC_CHER_CH1;
 	
 	//enable adc clk
-	enable_adc_clk();
-	
-	//Start ADC conversion
-	
-	
-	//read data register
-	//adc_ptr->ADC_CDR[0]
-	
+	enable_adc_clk();	
 }
 
